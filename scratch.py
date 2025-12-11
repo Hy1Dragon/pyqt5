f13863a7050e3f3c4ac900ee46ba77aa3f78b637
Hy1Dragon <mihailvasilev6090@gmail.com>
2025-12-11 19:50:45 +0300

    Add files via upload

---
+++ scratch.py
@@ -0,0 +1,10 @@
+from PyQt5.QtCore import Qt
+
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout
+app = QApplication([])
+main_win = QWidget()
+main_win.setWindowTitle('67')
+main_win.resize(400,200)
+main_win.show()
+app.exec_()
+
