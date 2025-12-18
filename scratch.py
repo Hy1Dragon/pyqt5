276705bd1c87c749749d507b67450d9e4d90ebaf
Hy1Dragon <mihailvasilev6090@gmail.com>
2025-12-18 19:57:35 +0300

    Update scratch.py

--- scratch.py
+++ scratch.py
@@ -1,27 +1,29 @@
 from PyQt5.QtCore import Qt
-
-
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout
+import random
+def SANIS():
+    a = ['(@_@)','54','456','745','47','45734','9']
+    asgor.setText(random.choice(a))
+
 app = QApplication([])
 main_win = QWidget()
 main_win.setWindowTitle('Underverse')
 main_win.resize(400,200)
 main_win.show()
-
-def SANIS():
-
-
 asgor=QLabel('(@_@):')
-asgor.setText('UNDERVERS')
-papirus = QPushButton('Pypsik')
 
+
+
+papirus = QPushButton('Pypsik')
 v_line = QVBoxLayout()
 
+v_line.addWidget(asgor,alignment = Qt.AlignCenter)
 
+v_line.addWidget(papirus,alignment = Qt.AlignCenter)
 
 main_win.setLayout(v_line)
 
-main_win.clicked.connect(SANIS)
+papirus.clicked.connect(SANIS)
 
 app.exec_()
 
